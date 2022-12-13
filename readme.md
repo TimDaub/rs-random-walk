aa4ddfbfd935a296a0fad2b7a0f9bc8f6442aedf
Tim Daubenschütz <tim@daubenschuetz.de>
2022-12-13 11:19:21 +0100

    Polish repository

---
+++ readme.md
@@ -0,0 +1,21 @@
+# rs-random-walk
+
+> Performs a random walk on a 2D plane and outputs a `.png` representation.
+
+## Output
+
+<p align="center">
+  <img src="/image.png" />
+</p>
+
+## Context
+
+With my last project, [neume-network](https://github.com/neume-network) I built
+a large project purely in node.js and enjoyed myself using it as a system
+engineering language. But I also noticed its short-comings and in general, I
+felt I wanted to be closer to the metal. So as an attempt of getting there,
+rs-random-walk is a first attempt at nearing.
+
+## License
+
+MIT
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use image::{ImageBuffer, Rgb, RgbImage};
+use image::{ ImageBuffer, Rgb, RgbImage };
 use imageproc::drawing::draw_line_segment_mut;
 use rand::Rng;
 
-fn walk_direction(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, x1: f32, y1: f32, x2: f32, y2: f32) {
+fn walk_direction(
+    img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
+    x1: f32,
+    y1: f32,
+    x2: f32,
+    y2: f32
+) {
     draw_line_segment_mut(img, (x1, y1), (x2, y2), Rgb([255, 255, 255]));
 }
 
@@ -16,36 +22,36 @@ fn main() {
 
     let mut x = 500f32;
     let mut y = 500f32;
-    for n in 0..50000 {
+    for _n in 0..50000 {
         // NOTE: 0:=up, 1:=right, ... (clock wise)
         let draw: u32 = rng.gen_range(0..4);
         if draw == 0 {
-            let new_y = y-step_size;
+            let new_y = y - step_size;
             if new_y < 0f32 {
                 continue;
             }
-            walk_direction(&mut img, x, y, x, y-step_size);
+            walk_direction(&mut img, x, y, x, y - step_size);
             y = new_y;
         } else if draw == 1 {
-            let new_x = x+step_size;
-            if new_x > width as f32 {
+            let new_x = x + step_size;
+            if new_x > (width as f32) {
                 continue;
             }
-            walk_direction(&mut img, x, y, x+step_size, y);
+            walk_direction(&mut img, x, y, x + step_size, y);
             x = new_x;
         } else if draw == 2 {
-            let new_y = y+step_size;
-            if new_y > height as f32 {
+            let new_y = y + step_size;
+            if new_y > (height as f32) {
                 continue;
             }
-            walk_direction(&mut img, x, y, x, y+step_size);
+            walk_direction(&mut img, x, y, x, y + step_size);
             y = new_y;
         } else if draw == 3 {
-            let new_x = x-step_size;
+            let new_x = x - step_size;
             if new_x < 0f32 {
                 continue;
             }
-            walk_direction(&mut img, x, y, x-step_size, y);
+            walk_direction(&mut img, x, y, x - step_size, y);
             x = new_x;
         }
     }
